fca2fa53c5d67c26914a5d663f073fb456a3c7aa
Harsh <Hvp6848@gmail.com>
2021-12-20 22:35:39 +0530

    Filtered code and added few notes.

--- app/src/main/java/com/example/guessmynumbergame/MainActivity.kt
+++ app/src/main/java/com/example/guessmynumbergame/MainActivity.kt
@@ -1,13 +1,19 @@
+/*
+This is a simple guessing game of numbers between 1 and 10, for android in kotlin
+Here I learned :-
+* A new library of synthetic which reduced my line of codes while referencing any id from XML to MainActivity Page.
+* I also learned about putting any text in any Text view by ( IDofVIEW.text ).
+* Learned to change visibility of the View of any view.
+*/
 package com.example.guessmynumbergame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var number = 5
+    private var number = 5
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,22 +21,26 @@ class MainActivity : AppCompatActivity() {
             String.format(resources.getString(R.string.game_question), number.toString())
     }
 
-    fun clickDownward(view: View) {
-        if (number == 5) {
-            number = 3
-            question()
-            game_top.text =
-                String.format(resources.getString(R.string.game_question), number.toString())
-        } else if (number == 3) {
-            number = 2
-            question()
-            game_top.text =
-                String.format(resources.getString(R.string.game_question), number.toString())
-        } else if (number == 2) {
-            number = 1
-            question()
-            game_top.text =
-                String.format(resources.getString(R.string.game_question), number.toString())
+    fun clickDownward(v: View) {
+        when (number) {
+            5 -> {
+                number = 3
+                question()
+                game_top.text =
+                    String.format(resources.getString(R.string.game_question), number.toString())
+            }
+            3 -> {
+                number = 2
+                question()
+                game_top.text =
+                    String.format(resources.getString(R.string.game_question), number.toString())
+            }
+            2 -> {
+                number = 1
+                question()
+                game_top.text =
+                    String.format(resources.getString(R.string.game_question), number.toString())
+            }
         }
         if (number == 8) {
             number = 7
@@ -45,22 +55,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun clickUpward(view: View) {
-        if (number == 5) {
-            number = 8
-            game_top.text =
-                String.format(resources.getString(R.string.game_question), number.toString())
-            question()
-        } else if (number == 8) {
-            number = 9
-            question()
-            game_top.text =
-                String.format(resources.getString(R.string.game_question), number.toString())
-        } else if (number == 9) {
-            number = 10
-            question()
-            game_top.text =
-                String.format(resources.getString(R.string.game_question), number.toString())
+    fun clickUpward(v:View) {
+        when (number) {
+            5 -> {
+                number = 8
+                game_top.text =
+                    String.format(resources.getString(R.string.game_question), number.toString())
+                question()
+            }
+            8 -> {
+                number = 9
+                question()
+                game_top.text =
+                    String.format(resources.getString(R.string.game_question), number.toString())
+            }
+            9 -> {
+                number = 10
+                question()
+                game_top.text =
+                    String.format(resources.getString(R.string.game_question), number.toString())
+            }
         }
         if (number == 3) {
             number = 4
@@ -70,7 +84,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun clickReset(view: View) {
+    fun clickReset(v: View) {
         number = 5
         game_result.visibility = View.INVISIBLE
         game_top.visibility = View.VISIBLE
@@ -78,13 +92,13 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun clickCheck(view: View) {
+    fun clickCheck(v: View) {
         game_top.visibility = View.INVISIBLE
         game_result.visibility = View.VISIBLE
         game_result.text =
             String.format(resources.getString(R.string.game_success), number.toString())
     }
-    fun question(){
+    private fun question(){
         String.format(resources.getString(R.string.game_question), number.toString())
     }
 }
